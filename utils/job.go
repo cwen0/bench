7621023456db885aa4dab91c6e8d7ed426fac26c
cwen0 <cwenyin0@gmail.com>
2016-10-27 00:18:58 +0800

    add handle sql

--- utils/job.go
+++ utils/job.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -27,5 +28,37 @@ func Waiting(doneChan chan struct{}, start time.Time, jobCount int, workerCount 
 	fmt.Printf("total %d cases, cost %d seconds, tps %d, start %s, now %s\n", jobCount, seconds, tps, start, now)
 }
 
-func HandleJob(db *sql.DB, testData []string, batch int, jobChan chan struct{}, doneChan chan struct{}) {
+func HandleJob(db *sql.DB, data []string, batch int, jobChan chan struct{}, doneChan chan struct{}) {
+	temp := 0
+	count := 0
+	for range jobChan {
+		temp++
+		if temp == batch {
+			doExec(db, data[count-batch+1:count+1])
+			temp = 0
+		}
+		count++
+	}
+	if temp > 0 {
+		temp = 0
+		doExec(db, data[count:len(data)])
+	}
+	doneChan <- struct{}{}
+}
+
+func doExec(db *sql.DB, data []string) {
+	txn, err := db.Begin()
+	if err != nil {
+		log.Fatalf("Transaction bengin Error: %s", err)
+	}
+	for _, sql := range data {
+		_, err := txn.Exec(sql)
+		if err != nil {
+			log.Fatalf("Exec sql Error: %s", err)
+		}
+	}
+	err = txn.Commit()
+	if err != nil {
+		log.Fatalf("Transcation commit Error: %s", err)
+	}
 }
